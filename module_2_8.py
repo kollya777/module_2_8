52694b66b235cc0005032b6aa00c931a1931760c
kollya777 <kollya777@mail.ru>
2024-06-09 18:26:33 +0300

    Add files via upload

---
+++ module_2_8.py
@@ -0,0 +1,12 @@
+def get_multiplied_digits(namber):
+    str_number = str(namber)
+    first = int(str_number[0])
+    if first == 0:
+        first = 1
+    elif len(str_number) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+result = get_multiplied_digits(40203)
+print(result)
